5b4d552d41b8644cb4793c97364d95368a43ec8d
UwUshka327 <S2000g.UwU@proton.me>
2026-09-08 16:18:38 +0300

    Reapply "UwUshka"
    This reverts commit 45583c2df878e68526eb74c82b0d800748b9698e.

--- Content.Goobstation.Client/Patron/PatronSupportSystem.cs
+++ Content.Goobstation.Client/Patron/PatronSupportSystem.cs
@@ -1,5 +1,4 @@
 using Content.Client.Lobby;
-using Content.Goobstation.Common.CCVar;
 using Content.Shared.CCVar;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controllers;
@@ -19,19 +18,6 @@ public sealed class PatronSupportUIController : UIController, IOnStateEntered<Lo
     {
         if (_hasShownThisSession)
             return;
-
-        var lastShown = _cfg.GetCVar(GoobCVars.PatronSupportLastShown);
-        var now = DateTime.UtcNow;
-
-        if (!string.IsNullOrEmpty(lastShown))
-        {
-            if (DateTime.TryParse(lastShown, out var lastShownDate))
-            {
-                var daysSinceLastShown = (now - lastShownDate).TotalDays;
-                if (daysSinceLastShown < _cfg.GetCVar(GoobCVars.PatronAskSupport))
-                    return;
-            }
-        }
 
         _hasShownThisSession = true;
         ShowSupportWindow();
@@ -57,18 +43,17 @@ public sealed class PatronSupportUIController : UIController, IOnStateEntered<Lo
 
         _supportWindow.PatreonButton.OnPressed += _ =>
         {
-            var patreonLink = _cfg.GetCVar(CCVars.InfoLinksPatreon);
-            if (!string.IsNullOrEmpty(patreonLink))
-                _uriOpener.OpenUri(new Uri(patreonLink));
+            var discordLink = _cfg.GetCVar(CCVars.InfoLinksDiscord);
+            if (!string.IsNullOrEmpty(discordLink))
+                _uriOpener.OpenUri(new Uri(discordLink));
+
             _supportWindow?.Close();
         };
 
-        _supportWindow.OpenCenteredLeft();
+        _supportWindow.OpenCentered();
     }
 
     private void OnWindowClosed()
     {
-        _cfg.SetCVar(GoobCVars.PatronSupportLastShown, DateTime.UtcNow.ToString("O"));
-        _cfg.SaveToFile();
     }
 }
